7f26741ac512602727e46fabb766936fb99ba09b
zankro <robert.zanc90@gmail.com>
2023-11-21 00:07:10 +0100

    Prime prove

---
+++ prova1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func main() {
+	deployment := appsv1.Deployment{
+		Spec: appsv1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Name: "web", Image: "nginx:1.21"},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Printf("%#v", &deployment)
+}
